396c019dfd7e8bffcf824ff33a5896c4a110f3a7
Itumeleng <mzobeitumeleng@gmail.com>
2023-07-20 16:57:16 -0700

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,15 +4,21 @@
  *
  * return: void
  */
-
 void print_most_numbers(void)
 {
-	int c;
+	int x = 0;
 
-	for (c = 0; c <= 9; c++)
+	for (; x <= 9; x++)
 	{
-		if ((c == 2 || c == 4))
-			_putchar(c);
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
 	}
+
 	_putchar('\n');
 }
