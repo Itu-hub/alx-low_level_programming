a781aff128fa6acf4a379affc41e01b850536e91
Itumeleng <mzobeitumeleng@gmail.com>
2023-07-20 14:43:14 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiples 2 integars
+ * @a: first integar
+ * @b: second integar
+ *
+ * Return: addition of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
